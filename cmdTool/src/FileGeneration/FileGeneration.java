14e7500a1380d4525d98258e20f83aa8442544b6
Qing Ye <yeqing@usc.edu>
2018-11-05 23:00:20 -0800

    Update FileGeneration.java

--- cmdTool/src/FileGeneration/FileGeneration.java
+++ cmdTool/src/FileGeneration/FileGeneration.java
@@ -1,11 +1,12 @@
-package FileGeneration;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FileGeneration {
 	private FileReader fr = null;
@@ -24,13 +25,25 @@ public class FileGeneration {
 			fw = new FileWriter("TestOutput.txt");
 			pw = new PrintWriter(fw);
 			String line = br.readLine();
+			JSONArray list = new JSONArray();
 			while(line!=null) {
-				writeLinkingResultsToFile(line, pw);
+				JSONObject obj = new JSONObject();
+				String[] splited = line.split("<");
+				String object = splited[1].substring(0, splited[1].length()-2);
+				obj.put("name: ",object);
+				String[] getRidOfSymble = splited[2].split("#");
+				String relationship = getRidOfSymble[1];
+				obj.put("relationship: ", relationship.substring(0, relationship.length()-2));
+				obj.put("data entity: ", splited[3].substring(0, splited[3].length()-3));
+				list.put(obj);
 				line=br.readLine();
 			}
+			pw.print(list);
 			pw.flush();
 		}catch(FileNotFoundException fnfe) {
 			System.out.println(fnfe.getMessage());
+		}catch(JSONException je) {
+			System.out.println(je.getMessage());
 		}catch(IOException ioe) {
 			System.out.println(ioe.getMessage());
 		}finally {
@@ -59,18 +72,9 @@ public class FileGeneration {
 		}
 		
 	}
-	public void writeLinkingResultsToFile(String line, PrintWriter pw) {
-		String[] splited = line.split("<");
-		String object = splited[1].substring(0, splited[1].length()-2);
-		pw.print(object+" ");
-		String[] getRidOfSymble = splited[2].split("#");
-		String relationship = getRidOfSymble[1];
-		pw.print(" is "+relationship.substring(0, relationship.length()-2));
-		pw.println(" "+splited[3].substring(0, splited[3].length()-3));
-	}
 	
-//	public static void main(String[] args) {
-//		FileGeneration fg = new FileGeneration();
-//		fg.GenerateFileOutput("test.txt");
-//	}
+	public static void main(String[] args) {
+		FileGeneration fg = new FileGeneration();
+		fg.GenerateFileOutput("test.txt");
+	}
 }
